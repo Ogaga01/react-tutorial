03c5154f0b25aeeb104c1977560f67cbf4a5325f
Ogaga <iyaraogaga69@gmail.com>
2022-05-30 12:21:21 +0100

    Add default todo list

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import TodosList from "./TodosList";
 class TodoContainer extends React.Component {
     state = {
         todos: [
@@ -21,13 +22,9 @@ class TodoContainer extends React.Component {
        };
   render() {
     return (
-      <React.Fragment>
-        <ul>
-          {this.state.todos.map(todo => (
-            <li>{todo.title}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+        <div>
+          <TodosList todos={this.state.todos} />
+        </div>
     )
   }
 }
---
+++ src/components/TodosList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class TodosList extends React.Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+export default TodosList
