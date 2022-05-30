0386a093fd811d736dfdf5f5a57c75430aac46f8
Ogaga <iyaraogaga69@gmail.com>
2022-05-30 13:36:22 +0100

    Add setState method

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ class TodoItem extends React.Component {
     render(){
         return (
             <li>
-              <input type="checkbox" checked={this.props.todo.completed} onChange={() => console.log("clicked")}/> {this.props.todo.title}
+              <input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)}/> {this.props.todo.title}
             </li>
         )
     }
