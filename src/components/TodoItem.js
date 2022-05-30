3572fc139210362721db5679f2520d7bc84e06d1
Ogaga <iyaraogaga69@gmail.com>
2022-05-30 13:16:51 +0100

    Add handler to input

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,10 @@ import React from "react";
 
 class TodoItem extends React.Component {
     render(){
-        return(
-            <div><li>{this.props.todo.title}</li></div>
+        return (
+            <li>
+              <input type="checkbox" checked={this.props.todo.completed} onChange={() => console.log("clicked")}/> {this.props.todo.title}
+            </li>
         )
     }
 }
