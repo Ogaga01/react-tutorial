e3031a8fe998d7ef490b17ee719788b96b1e1d2b
Ogaga <iyaraogaga69@gmail.com>
2022-05-30 12:31:17 +0100

    Add key values for all todo

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -3,7 +3,11 @@ import React from "react";
 class TodosList extends React.Component {
     render(){
         return(
-            <div></div>
+            <ul>
+            {this.props.todos.map(todo => (
+              <li key={todo.id}>{todo.title}</li>
+            ))}
+          </ul>
         )
     }
 }
