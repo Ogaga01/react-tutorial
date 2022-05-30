d1c443cb46ca80c73924a099023e96eca336ffc1
Ogaga <iyaraogaga69@gmail.com>
2022-05-30 12:45:01 +0100

    Add todoitem component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+    render(){
+        return(
+            <div><li>{this.props.todo.title}</li></div>
+        )
+    }
+}
+
+export default TodoItem
